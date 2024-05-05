4a8549fc64b27b976a4a466ed1e79ccc0156c898
Karan Rajbhar <kran@outlook.com>
2024-05-05 21:18:22 +0530

    added init

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Karan Rajbhar on 04/05/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+    }
+}
